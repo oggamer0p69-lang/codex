eff9c04000d37b0c4220b3a1667535298468ccb2
RacchuDevadiga <rac>
2025-12-04 13:24:09 +0530

    landing page

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react' // <-- 1. Import the React plugin
 
 export default defineConfig({
   plugins: [
+    react(), // <-- 2. Use the React plugin
     tailwindcss(),
   ],
 })
